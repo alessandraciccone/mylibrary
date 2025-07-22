dedcf63dd02e8efc8316f2d28c775aa74b136d2e
alessandraciccone <alessandraciccone@icloud.com>
2025-07-22 17:07:56 +0200

    .

--- src/assets/Components/MyFooter.jsx
+++ src/assets/Components/MyFooter.jsx
@@ -2,7 +2,7 @@ const MyFooter = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-dark">
+    <footer className="bg-dark h-100">
       <p className=" text-light text-center">
         Nebbia delle Rune- {currentYear}
       </p>
