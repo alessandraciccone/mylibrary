2613a4b2a353a93342f9191b7cc9d657c13f1f45
alessandraciccone <alessandraciccone@icloud.com>
2025-07-23 09:33:28 +0200

    adds comment

--- src/assets/Components/AllBooks.jsx
+++ src/assets/Components/AllBooks.jsx
@@ -14,6 +14,7 @@ const AllBooks = () => {
         </Col>
       </Row>
       <Row className="justify-content-center">
+        {/*ora uso map ma uso slice per prendere solo qualche oggetto dell'arra*/}
         {book.slice(0, 24).map((libri) => {
           return (
             <Col sm={6} md={4} lg={3} className="d-flex m-4 mt-4">
@@ -21,19 +22,21 @@ const AllBooks = () => {
                 key={libri.asin}
                 className="d-flex flex-column h-100 bg-transparent border-1 border-secondary p-2"
               >
+                {/* metto la key altrimenti react non riesce a gestire l'array. la key deve essere sempre un elemento univoco di ogni oggetto, in questo caso l'asin che trovo nellì'array*/}
                 <Card.Img
                   variant="top"
                   src={libri.img}
                   alt={libri.title}
                   className="card-image"
-                />
+                />{" "}
+                {/*prendo l'immagine e prendo il titolo dell'immagine x la descrizione*/}
                 <Card.Body className="d-flex flex-column justify-content-between">
                   <div>
                     <Card.Title className="text-center text-white">
-                      {libri.title}
+                      {libri.title} {/*prendo il titolo dellibro*/}
                     </Card.Title>
                     <Card.Text className="text-center text-white">
-                      {libri.price} €
+                      {libri.price} € {/*prendo il prezzo*/}
                     </Card.Text>
                   </div>
                   <div className="d-flex justify-content-center mt-auto">
